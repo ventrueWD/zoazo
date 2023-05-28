5f063a025ebc3fff9f89cf74567747ba36d0b720
ventrueWD <134661258+ventrueWD@users.noreply.github.com>
2023-05-27 19:19:10 -0500

    Create zoazo_gen.js

---
+++ zoazo_gen.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+function getRandomWordFromFile() {
+  // Get the current script file directory
+  const scriptDirectory = path.dirname(__filename);
+
+  // Construct the file path for "words.txt"
+  const filePath = path.join(scriptDirectory, 'words.txt');
+
+  // Read the contents of the file
+  const fileContents = fs.readFileSync(filePath, 'utf8');
+
+  // Split the file contents into an array of words
+  const words = fileContents.split('\n');
+
+  // Remove any empty or whitespace-only words
+  const filteredWords = words.filter((word) => word.trim() !== '');
+
+  // Remove words with less than three letters
+  const filteredWordsWithThreeLetters = filteredWords.filter((word) => word.length >= 3);
+
+  // Generate a random index
+  const randomIndex = Math.floor(Math.random() * filteredWordsWithThreeLetters.length);
+
+  // Retrieve the random word
+  const randomWord = filteredWordsWithThreeLetters[randomIndex];
+
+  return randomWord;
+}
+
+// Usage example
+const word = getRandomWordFromFile();
+console.log(word);
